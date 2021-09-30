6756d5759a8a7596cce493b8718a8a130e2880af
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-09-30 12:13:33 -0500

    add config dataset path and name - add percentages

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 config = {
-    'dataset':"data/creditcard.csv"
+    'dataset_source':"data/creditcard.csv",
+    'dataset_preprocessed':"data/creditcard_preprocessed.csv",
+    'dataset_x_train':"data/dataset_x_train.csv",
+    'dataset_x_test':"data/dataset_x_test.csv",
+    'dataset_y_train':"data/dataset_y_train.csv",
+    'dataset_y_test':"data/dataset_y_test.csv",
+    'pct_data_train': 0.8, # 80%
+    'pct_data_test': 0.2 # 20%
 }
