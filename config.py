8750e75a252e05e24b2a8ab38928d1a860ce1a03
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-09-30 14:23:07 -0500

    add config parameters

--- config.py
+++ config.py
@@ -6,5 +6,13 @@ config = {
     'dataset_y_train':"data/dataset_y_train.csv",
     'dataset_y_test':"data/dataset_y_test.csv",
     'pct_data_train': 0.8, # 80%
-    'pct_data_test': 0.2 # 20%
+    'pct_data_test': 0.2, # 20%
+    'nodes_input': 37,
+    'multiplier':1.5,
+    'nodes_layer_hidden': 18,
+    'training_epochs': 5, # 2000
+    'training_dropout': 0.9,
+    'display_step': 1, # 10
+    'batch_size': 2048,
+    'learning_rate': 0.005
 }
